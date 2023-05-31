a5d8d1a5568c2b18f0fd3bc706e677ef0659c3d1
msanmillan <msanmillan@serbatic.es>
2023-05-31 23:02:28 +0200

    Standalone Components

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { MapsRoutingModule } from './maps-routing.module';
 import { MiniMapComponent } from './components/mini-map/mini-map.component';
-import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
 import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-page.component';
 import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
@@ -12,13 +11,14 @@ import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.
 
 
 import * as mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+import { CounterAloneComponent } from '../alone/components/counter-alone/counter-alone.component';
+import { SideMenuComponent } from '../alone/components/side-menu/side-menu.component';
 (mapboxgl as any).accessToken  ='pk.eyJ1IjoibXNhbm1pbGxhbiIsImEiOiJjbGRza2hnaGkwb29oM25rZzBhenV2eWl5In0.C7btNoW-u5eU9w2HOEWyNw';
 
 
 @NgModule({
   declarations: [
     MiniMapComponent,
-    SideMenuComponent,
     MapsLayoutComponent,
     FullScreenPageComponent,
     MarkersPageComponent,
@@ -27,7 +27,9 @@ import * as mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-g
   ],
   imports: [
     CommonModule,
-    MapsRoutingModule
+    MapsRoutingModule,
+    CounterAloneComponent,
+    SideMenuComponent
   ]
 })
 export class MapsModule { }
